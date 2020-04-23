c7b38be8a17d6caf260453b0c4ccdd8d6001c265
fujioka <fujioka@ustec.co.jp>
2020-04-23 14:52:36 +0900

    [add]ソリューションを作成

---
+++ ProgrammingTraining/ProgrammingTraining.Worklist/WorklistModule.cs
@@ -0,0 +1,20 @@
+﻿using ProgrammingTraining.Worklist.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ProgrammingTraining.Worklist
+{
+    public class WorklistModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
