c8a4c1026d09ce67a8d07b5bcb89de15d75f012c
fujioka <fujioka@ustec.co.jp>
2020-06-03 21:03:08 +0900

    [add]Login画面実装

--- ProgrammingTraining.Login/ViewModels/LoginViewModel.cs
+++ ProgrammingTraining.Login/ViewModels/LoginViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Mvvm;
 using Prism.Regions;
 using Reactive.Bindings;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ProgrammingTraining.Login.ViewModels
@@ -13,11 +14,18 @@ namespace ProgrammingTraining.Login.ViewModels
 
         public ReactiveCommand<PasswordBox> LoginCommand { get; }
 
+        public ReactiveCommand ShutdownCommand { get; }
+
         private void Login(PasswordBox passwordBox)
         {
             passwordBox.Password = string.Empty;
             // TODO
             this._regionManager.RequestNavigate("ContentRegion", nameof(Worklist.Views.Worklist));
+        }
+
+        private void Shutdown()
+        {
+            Application.Current.Shutdown();
         }
 
         public LoginViewModel(IRegionManager regionManager)
@@ -26,6 +34,9 @@ namespace ProgrammingTraining.Login.ViewModels
 
             this.LoginCommand = new ReactiveCommand<PasswordBox>()
                 .WithSubscribe(passwordBox => this.Login(passwordBox));
+
+            this.ShutdownCommand = new ReactiveCommand()
+                .WithSubscribe(this.Shutdown);
         }
     }
 }
