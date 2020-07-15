9972e9c062f0cb1237208e13fa997162b52d913e
fujioka <fujioka@ustec.co.jp>
2020-07-15 15:56:24 +0900

    [add]Shellのタイトル変更機能を追加

---
+++ ProgrammingTraining.Models/TitleMessenger.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingTraining.Models
+{
+    /// <summary>
+    /// Shell(MainWindow)のTitleのModel
+    /// </summary>
+    public class TitleMessenger : BindableBase
+    {
+        private string title = "PRISM APP";
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        public TitleMessenger()
+        {
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="title"></param>
+        public void ChangeTitle(string title)
+        {
+            this.Title = title;
+        }
+    }
+}
