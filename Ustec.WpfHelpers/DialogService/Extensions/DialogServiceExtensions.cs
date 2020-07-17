3125bfe668851623bed2ceef5fb93dd41418bff0
fujioka <fujioka@ustec.co.jp>
2020-07-17 10:24:10 +0900

    [add]PRISM用DialogServiceを追加

---
+++ Ustec.WpfHelpers/DialogService/Extensions/DialogServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Services.Dialogs;
+
+namespace Ustec.WpfHelpers.DialogService.Extensions
+{
+    public static class DialogServiceExtensions
+    {
+        /// <summary>問い合わせメッセーボックスを表示します。</summary>
+        /// <param name="dialogService">IDialogService。</param>
+        /// <param name="message">表示するメッセージを表す文字列。</param>
+        /// <returns>問い合わせメッセーボックスの戻り値を表すButtonResult列挙型の内の1つ。</returns>
+        public static ButtonResult ShowConfirmationMessage(this IDialogService dialogService, string message)
+        {
+            var param = new DialogParameters($"Message={message}");
+            var tempRet = ButtonResult.Cancel;
+
+            dialogService.ShowDialog("ConfirmedMessageBox", param, r => tempRet = r.Result);
+
+            return tempRet;
+        }
+    }
+}
---
+++ Ustec.WpfHelpers/DialogService/Views/MahappsWindowForDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MahApps.Metro.Controls;
+using Prism.Services.Dialogs;
+
+namespace Ustec.WpfHelpers.DialogService.Views
+{
+    /// <summary>
+    /// Interaction logic for MahappsWindowForDialog.xaml
+    /// </summary>
+    public partial class MahappsWindowForDialog : MetroWindow, IDialogWindow
+    {
+        public MahappsWindowForDialog()
+        {
+            InitializeComponent();
+        }
+
+        public IDialogResult Result { get; set; }
+    }
+}
